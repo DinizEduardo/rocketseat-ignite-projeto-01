63be38a78ee61ee97f178bf16f3c120a1a350efb
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-06-29 23:50:46 -0300

    feat: utilizando o cookie nas requisições de get

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,29 +2,56 @@ import { FastifyInstance } from 'fastify'
 import crypto, { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middleware/check-session-id-existis'
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-    return summary
-  })
+      const summary = await knex('transactions')
+        .where({
+          session_id: sessionId,
+        })
+        .sum('amount', { as: 'amount' })
+        .first()
 
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+      return summary
+    },
+  )
 
-    return { transactions }
-  })
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
 
-  app.get('/:id', async (request) => {
+      const transactions = await knex('transactions')
+        .where({
+          session_id: sessionId,
+        })
+        .select()
+
+      return { transactions }
+    },
+  )
+
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const { sessionId } = request.cookies
+
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
 
     return { transaction }
   })
