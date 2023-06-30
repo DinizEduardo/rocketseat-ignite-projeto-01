82223d5151b7b13f47b28693fc3bbcf66f2b4a94
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-06-30 14:19:20 -0300

    test: criando o teste para validar uma nova transação

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['DEV', 'TEST', 'PROD']).default('PROD'),
+  NODE_ENV: z.enum(['DEV', 'test', 'PROD']).default('PROD'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
