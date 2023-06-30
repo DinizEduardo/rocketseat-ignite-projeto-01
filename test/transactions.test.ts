b6230a8b6f3a700573be3b30621ab54637d68f12
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-06-30 20:30:01 -0300

    test: criado restante dos testes e2e

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -51,4 +51,62 @@ describe('Transactions Routes', () => {
       }),
     ])
   })
+
+  test('should be get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transação',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Nova transação',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({ amount: 3000 })
+  })
+
+  test('should be able to get a specific transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transação',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTrtansactionResponse = await request(app.server)
+      .get('/transactions/' + transactionId)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTrtansactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Nova transação',
+        amount: 5000,
+      }),
+    )
+  })
 })
